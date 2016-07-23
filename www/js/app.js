0333414acbf96d9446533fea7efc62c3cc8f028d
lodhi13 <ammadlodhi_13@yahoo.com>
2016-07-23 18:55:42 +0500

    Application Architecture
    Making the basic architecture of the application so that once it is done
    only pages would bhe left to design

--- www/js/app.js
+++ www/js/app.js
@@ -24,3 +24,18 @@ angular.module('starter', ['ionic'])
     }
   });
 });
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/Login.html',
+    controller: 'AppCtrl'
+  })
+
+    $urlRouterProvider.otherwise('/app/Login');
+  });
+
+
